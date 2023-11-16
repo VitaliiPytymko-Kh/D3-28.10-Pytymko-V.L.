30bb7c29be7f47134c603498bf0a3bc95ae0fc53
VitaliiPytymko <vitaliipytymko@gmail.com>
2023-11-16 23:26:13 +0200

    Добавьте файлы проекта.

---
+++ D3 28.10 Pytymko V.L/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D3_28._10_Pytymko_V.L_
+{
+    //Завдання 2
+    //Створіть набір методів:
+    //■ Метод відображення поточного часу;
+    //■ Метод відображення поточної дати;
+    //■ Метод відображення поточного дня тижня;
+    //■ Метод для підрахунку площі трикутника;
+    //■ Метод для підрахунку площі прямокутника.
+    //Для реалізації проєкту використовуйте делегати: Action,
+    //Predicate, Func.
+    class Program
+    {
+
+        static void _Time()
+        { Console.WriteLine($"Поточний час: {DateTime.Now.ToShortTimeString()}"); }
+
+        static void _Date()
+        { Console.WriteLine($"Поточна дата: {DateTime.Now.ToShortDateString()}"); }
+
+        static void _DayOfWeek()
+        { Console.WriteLine($"Поточний день тижня: {DateTime.Now.DayOfWeek}"); }
+
+        static double _TriangleArea(double a, double b, double c)
+        {
+            double s = (a + b + c) / 2;
+            return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+        }
+
+        static double _RectangleArea(double length, double width)
+        { return length * width; }
+        static void Main()
+        {
+
+            Action nT = _Time;
+            nT.Invoke();
+
+            Action nD = _Date;
+            nD.Invoke();
+
+            Action cDoW = _DayOfWeek;
+            cDoW.Invoke();
+
+            Func<double, double, double, double> TA = _TriangleArea;
+            double ta = TA.Invoke(3, 4, 5);
+            Console.WriteLine($"Площа трикутника: {ta}");
+
+            Func<double, double, double> RA = _RectangleArea;
+            double ra = RA.Invoke(6, 8);
+            Console.WriteLine($"Площа прямокутника: {ra}");
+        }
+
+    }
+
+}
---
+++ D3 28.10 Pytymko V.L/Program.cs
@@ -0,0 +1,96 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+
+//Завдання 1
+//Створіть набір методів для роботи з масивами:
+//■ Метод для отримання усіх парних чисел у масиві;
+//■ Метод для отримання усіх непарних чисел у масиві;
+//■ Метод для отримання усіх простих чисел у масиві;
+//■ Метод для отримання усіх чисел Фібоначчі в масиві.
+//Використовуйте механізми делегатів
+
+
+class Program
+{
+
+    delegate bool MDA(int number);
+    static void Print(int[] numbers, MDA t3)
+    {
+        foreach (var number in numbers)
+        {
+            if (t3.Invoke(number))
+            {
+                Console.Write(number + " ");
+            }
+        }
+        Console.WriteLine();
+    }
+
+    static bool IsEven(int number)
+    {
+        return number % 2 == 0;
+    }
+
+    static bool IsOdd(int number)
+    {
+        return number % 2 != 0;
+    }
+
+    static bool IsPrime(int number)
+    {
+        if (number < 2)
+            return false;
+
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    static bool IsFibonacci(int number)
+    {
+        int a = 0, b = 1;
+        while (b < number)
+        {
+            int temp = a;
+            a = b;
+            b = temp + b;
+        }
+        return b == number;
+    }
+
+
+    static void Main()
+    {
+        MDA[] dg = { IsEven, IsOdd, IsPrime, IsFibonacci };
+        int[] MyArr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+        int choice = 0;
+        Console.WriteLine("Масив для фильтрування: ");
+        foreach (var i in MyArr)
+        {
+            Console.Write($"{i} ");
+        };
+        Console.WriteLine();
+
+        while (choice != 5)
+        {
+            Console.Write("\n1 IsEven \n2 IsOdd \n3 IsPrime \n4 IsFibonacci \n5 Exit\nYour choice: ");
+            choice = Convert.ToInt32(Console.ReadLine());
+            if (choice >= 1 && choice <= 4)
+            {
+                Print(MyArr, dg[choice - 1]);
+
+            }
+
+        }
+
+
+
+
+    }
+}
+  
